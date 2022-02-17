22932ff8b3cebafab8e07fa3558ea1834726b6c0
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-02-17 17:44:14 +0100

    Delete everyting beyond webrtc/videoroom_advanced directory

--- rtp/receive.exs
+++
@@ -1,7 +0,0 @@
-alias Membrane.Demo.RTP.ReceivePipeline
-{:ok, pid} = ReceivePipeline.start_link(%{
-    secure?: "--secure" in System.argv(),
-    video_port: 5000,
-    audio_port: 5002
-  })
-ReceivePipeline.play(pid)

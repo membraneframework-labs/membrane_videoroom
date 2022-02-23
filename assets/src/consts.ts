f3f2be9d02161092d53b84fea5fd4471aac907dd
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-02-23 11:51:31 +0100

    Bugfix

--- assets/src/consts.ts
+++ assets/src/consts.ts
@@ -16,7 +16,7 @@ export const SCREENSHARING_MEDIA_CONSTRAINTS: DisplayMediaStreamConstraints = {
 
 export const LOCAL_PEER_ID = "local-peer";
 
-const MBps = 1024 * 1024 * 8;
+const MBps = 1024 * 8;
 
 export const BANDWIDTH_LIMITS = {
   video: 10 * MBps,

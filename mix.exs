c09e365b56fb2fba56f44654ea94c062166616da
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-02-25 09:41:37 +0100

    Updating deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule VideoRoom.MixProject do
 
   defp deps do
     [
-      {:membrane_rtc_engine,
-       github: "membraneframework/membrane_rtc_engine", branch: "limit-bandwidth"},
+      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine"},
       {:membrane_webrtc_plugin, github: "membraneframework/membrane_webrtc_plugin"},
       {:plug_cowboy, "~> 2.5.2"},
       {:phoenix, "~> 1.6"},

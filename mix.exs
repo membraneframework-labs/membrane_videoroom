b1ff68caa72f2e059bc6f44733d78e1b0a090f67
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-03-25 16:30:56 +0100

    Set deps to master branches

--- mix.exs
+++ mix.exs
@@ -21,12 +21,9 @@ defmodule VideoRoom.MixProject do
 
   defp deps do
     [
-      {:membrane_rtc_engine,
-       github: "membraneframework/membrane_rtc_engine", branch: "join-empty-room-without-media"},
+      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine"},
       {:membrane_webrtc_plugin,
-       github: "membraneframework/membrane_webrtc_plugin",
-       branch: "join-empty-room-without-media",
-       override: true},
+       github: "membraneframework/membrane_webrtc_plugin", override: true},
       {:plug_cowboy, "~> 2.5.2"},
       {:phoenix, "~> 1.6"},
       {:phoenix_html, "~> 3.0"},

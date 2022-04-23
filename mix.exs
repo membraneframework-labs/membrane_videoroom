6ac9b5c1356e3a434c19cf29c0c60414af245425
Michał Śledź <sledzson08@gmail.com>
2022-04-23 17:29:58 +0200

    Adjust to new simulcast config. Remove things related to libnice. Update deps.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule VideoRoom.MixProject do
 
   defp deps do
     [
-      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine"},
+      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine", branch: "update-deps"},
       {:plug_cowboy, "~> 2.5.2"},
       {:phoenix, "~> 1.6"},
       {:phoenix_html, "~> 3.0"},

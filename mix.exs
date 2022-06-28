e3229f5138407a58e7f2bef7b377093fa5da39a3
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-06-28 14:25:26 +0200

    Update dependency to opentelemetry

--- mix.exs
+++ mix.exs
@@ -34,10 +34,10 @@ defmodule VideoRoom.MixProject do
       {:cowlib, "~> 2.11.0", override: true},
 
       # Otel
-      {:opentelemetry, "~> 0.6.0", override: true},
-      {:opentelemetry_api, "~> 0.6.0", override: true},
-      {:opentelemetry_exporter, "~> 0.6.0"},
-      {:opentelemetry_zipkin, "~> 0.4.0"},
+      {:opentelemetry, "~> 1.0"},
+      {:opentelemetry_api, "~> 1.0"},
+      {:opentelemetry_exporter, "~> 1.0"},
+      {:opentelemetry_zipkin, "~> 1.0"},
 
       # Benchmarks
       {:beamchmark, "~> 0.1.0", only: :benchmark},

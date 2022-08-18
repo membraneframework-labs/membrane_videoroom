a81a370be8d51e4892e4afe9cb56a545133ea184
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-08-18 16:51:07 +0200

    Delete child_spec() from MetricsPersistor

--- lib/videoroom/metrics_persistor.ex
+++ lib/videoroom/metrics_persistor.ex
@@ -3,34 +3,28 @@ defmodule VideoRoom.MetricsPersistor do
 
   alias Membrane.RTC.Engine.TimescaleDB.Reporter
 
+  @type option() :: GenServer.option() | {:reporter, Reporter.reporter()}
+  @type options() :: [option()]
+
   # 1 second
   @scrape_interval 1000
 
   # 1 hour
   @cleanup_interval 1000 * 60 * 60
 
-  @spec start(Reporter.reporter(), GenServer.options()) :: GenServer.on_start()
-  def start(reporter, options \\ []) do
+  @spec start(options()) :: GenServer.on_start()
+  def start(options \\ []) do
     do_start(:start, reporter, options)
   end
 
-  @spec start_link(Reporter.reporter(), GenServer.options()) :: GenServer.on_start()
-  def start_link(reporter, options \\ []) do
-    do_start(:start_link, reporter, options)
+  @spec start_link(options()) :: GenServer.on_start()
+  def start_link(options \\ []) do
+    do_start(:start_link, options)
   end
 
-  defp do_start(function, reporter, options) do
-    apply(GenServer, function, [__MODULE__, reporter, options])
-  end
-
-  @spec child_spec(Keyword.t()) :: Supervisor.child_spec()
-  def child_spec(opts) do
-    {reporter, process_opts} = Keyword.pop(opts, :reporter, nil)
-
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, [reporter, process_opts]}
-    }
+  defp do_start(function, options) do
+    {reporter, gen_server_options} = Keyword.pop(options, :reporter)
+    apply(GenServer, function, [__MODULE__, reporter, gen_server_options])
   end
 
   @impl true

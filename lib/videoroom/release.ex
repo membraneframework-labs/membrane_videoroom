f4a7e6c0c402ea4b143fa91ae0a9b3c25521d0b4
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-09-26 11:41:14 +0200

    Refactor Release module due to credo issues

--- lib/videoroom/release.ex
+++ lib/videoroom/release.ex
@@ -5,27 +5,26 @@ defmodule VideoRoom.Release do
   """
   @app :membrane_videoroom_demo
 
-  def migrate_and_start do
-    load_app()
-
-    for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-    end
-
-    Application.ensure_started(@app)
-  end
-
+  @spec migrate() :: :ok
   def migrate do
     load_app()
 
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _fun_return, _apps} =
+        Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
+
+    :ok
   end
 
+  @spec rollback(module(), integer()) :: :ok
   def rollback(repo, version) do
     load_app()
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+
+    {:ok, _fun_return, _apps} =
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+
+    :ok
   end
 
   defp repos do

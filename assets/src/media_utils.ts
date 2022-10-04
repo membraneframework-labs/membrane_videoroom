0b34a4a025b76dac6a6ba0812a18d0fa6d269722
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-10-04 11:34:47 +0200

    CR suggestions

--- assets/src/media_utils.ts
+++ assets/src/media_utils.ts
@@ -27,12 +27,35 @@ export async function getVideoStream(): Promise<MediaStream> {
     }
   }
 
-  return Promise.reject(new Error("No input devices are available"));
+  throw new Error("No input devices are available");
 }
 
 export async function getAudioStream(): Promise<MediaStream> {
-  // TODO: this should probably follow the same flow as `getVideoStream`
-  return await navigator.mediaDevices.getUserMedia({
-    audio: AUDIO_TRACK_CONSTRAINTS,
-  });
+  const constraints = {
+    video: AUDIO_TRACK_CONSTRAINTS,
+  };
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  } catch (_exception) {
+    const devices = (await navigator.mediaDevices.enumerateDevices()).filter(
+      (device) => device.kind === "audioinput"
+    );
+
+    for (let device of devices) {
+      try {
+        constraints.video.deviceId = { exact: device.deviceId };
+        return await navigator.mediaDevices.getUserMedia(constraints);
+      } catch (error) {
+        console.error(
+          "Failed to acquire a stream from deviceID ",
+          device.deviceId,
+          " due to ",
+          error
+        );
+      }
+    }
+  }
+
+  throw new Error("No input devices are available");
 }

cac5189880a52a8b9e27dd7bd0571322fa3f6324
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-10-04 15:23:18 +0200

    Refactor `media_utils.ts`

--- assets/src/media_utils.ts
+++ assets/src/media_utils.ts
@@ -1,50 +1,32 @@
 import { AUDIO_TRACK_CONSTRAINTS, VIDEO_TRACK_CONSTRAINTS } from "./consts";
 
 export async function getVideoStream(): Promise<MediaStream> {
-  const constraints = {
-    video: VIDEO_TRACK_CONSTRAINTS,
-  };
-
-  try {
-    return await navigator.mediaDevices.getUserMedia(constraints);
-  } catch (_exception) {
-    const devices = (await navigator.mediaDevices.enumerateDevices()).filter(
-      (device) => device.kind === "videoinput"
-    );
-
-    for (let device of devices) {
-      try {
-        constraints.video.deviceId = { exact: device.deviceId };
-        return await navigator.mediaDevices.getUserMedia(constraints);
-      } catch (error) {
-        console.error(
-          "Failed to acquire a stream from deviceID ",
-          device.deviceId,
-          " due to ",
-          error
-        );
-      }
-    }
-  }
-
-  throw new Error("No input devices are available");
+  return await getMedia(VIDEO_TRACK_CONSTRAINTS, "videoinput");
 }
 
 export async function getAudioStream(): Promise<MediaStream> {
+  return await getMedia(AUDIO_TRACK_CONSTRAINTS, "audioinput");
+}
+
+async function getMedia(
+  track_constraints: MediaTrackConstraints,
+  type: "audioinput" | "videoinput"
+): Promise<MediaStream> {
   const constraints = {
-    video: AUDIO_TRACK_CONSTRAINTS,
+    audio: type === "audioinput" ? track_constraints : false,
+    video: type === "videoinput" ? track_constraints : false,
   };
 
   try {
     return await navigator.mediaDevices.getUserMedia(constraints);
   } catch (_exception) {
     const devices = (await navigator.mediaDevices.enumerateDevices()).filter(
-      (device) => device.kind === "audioinput"
+      (device) => device.kind === type
     );
 
     for (let device of devices) {
       try {
-        constraints.video.deviceId = { exact: device.deviceId };
+        track_constraints.deviceId = { exact: device.deviceId };
         return await navigator.mediaDevices.getUserMedia(constraints);
       } catch (error) {
         console.error(

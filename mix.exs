4fc8841510086d8e9f91b71f9262ec1572216bc8
Michał Śledź <michalsledz34@gmail.com>
2022-12-01 14:04:13 +0100

    Update RTC Engine to 0.8.0 (#62)

--- mix.exs
+++ mix.exs
@@ -28,9 +28,12 @@ defmodule VideoRoom.MixProject do
 
   defp deps do
     [
-      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine", override: true},
-      {:membrane_rtc_engine_timescaledb,
-       github: "membraneframework/membrane_rtc_engine_timescaledb", runtime: false},
+      {:membrane_rtc_engine, "~> 0.8.0"},
+      {:membrane_rtp_plugin,
+       github: "membraneframework/membrane_rtp_plugin",
+       branch: "feature/outbound-retransmissions",
+       override: true},
+      {:membrane_rtc_engine_timescaledb, "~> 0.1.0", runtime: false},
       {:plug_cowboy, "~> 2.5.2"},
       {:phoenix, "~> 1.6"},
       {:phoenix_html, "~> 3.0"},

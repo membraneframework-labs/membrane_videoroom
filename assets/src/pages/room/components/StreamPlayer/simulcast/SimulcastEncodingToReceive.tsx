bbc4eb5c4f05325b94b58662056d8e73d076a072
Kamil Stasiak <kamilstasiaknew@gmail.com>
2023-02-15 15:50:04 +0100

    Update assets/src/pages/room/components/StreamPlayer/simulcast/SimulcastEncodingToReceive.tsx
    Co-authored-by: Bartosz Błaszków <bartosz.blaszkow@swmansion.com>

--- assets/src/pages/room/components/StreamPlayer/simulcast/SimulcastEncodingToReceive.tsx
+++ assets/src/pages/room/components/StreamPlayer/simulcast/SimulcastEncodingToReceive.tsx
@@ -17,9 +17,9 @@ export const SimulcastEncodingToReceive: FC<Props> = ({
 }: Props) => {
   return (
     <div className="absolute bottom-0 right-0 z-50 bg-white p-2 text-sm text-gray-700 opacity-80 md:text-base">
-      <div>Target encoding: {targetEncoding}</div>
+      <div>Target: {targetEncoding}</div>
       <div>
-        <label>Highest encoding to receive</label>
+        <label>Target cap: </label>
         <select
           disabled={disabled}
           value={userSelectedEncoding || undefined}

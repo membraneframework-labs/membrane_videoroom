98eaf85dcc9aa7fc7956c7f4be7bf63bc66318a6
Michał Śledź <michalsledz34@gmail.com>
2023-02-16 12:07:40 +0100

    Rename `Show simulcast controls` to `Nerd options`

--- assets/src/pages/room/RoomPage.tsx
+++ assets/src/pages/room/RoomPage.tsx
@@ -115,7 +115,7 @@ const RoomPage: FC<Props> = ({ roomId, displayName, isSimulcastOn, manualMode, a
               className="focus:outline-none focus:shadow-outline m-1 w-full rounded bg-brand-grey-80 py-2 px-4 text-white hover:bg-brand-grey-100"
               type="submit"
             >
-              Show simulcast controls
+              Nerd options
             </button>
           )}
         </div>

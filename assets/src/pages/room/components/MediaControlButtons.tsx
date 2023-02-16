2bb807cc1216762746300a1a2eb7148b206fb07b
alexmaar <56414498+alexmaar@users.noreply.github.com>
2023-02-16 10:06:44 +0100

    fix empty gap (#123)
    * fix empty gap
    
    * use boolean prop to hide screensharing controls
    
    * remove unused id props

--- assets/src/pages/room/components/MediaControlButtons.tsx
+++ assets/src/pages/room/components/MediaControlButtons.tsx
@@ -11,7 +11,6 @@ import Camera from "../../../features/room-page/icons/Camera";
 import CameraOff from "../../../features/room-page/icons/CameraOff";
 import Screenshare from "../../../features/room-page/icons/Screenshare";
 import HangUp from "../../../features/room-page/icons/HangUp";
-import clsx from "clsx";
 import useToast from "../../../features/shared/hooks/useToast";
 import { ToastType } from "../../../features/shared/context/ToastContext";
 
@@ -85,20 +84,22 @@ const getAutomaticControls = (
       },
   displayMedia.stream
     ? {
-        id: "stream-stop",
+        id: "screenshare-stop",
         icon: Screenshare,
         hover: "Stop sharing your screen",
-        className: clsx(neutralButtonStyle, "screensharing-control"),
+        className: neutralButtonStyle,
+        hideOnMobile: true,
         onClick: () => {
           displayMedia.stop();
           screenSharingStreaming.setActive(false);
         },
       }
     : {
-        id: "stream-start",
+        id: "screenshare-start",
         icon: Screenshare,
         hover: "Share your screen",
-        className: clsx(neutralButtonStyle, "screensharing-control"),
+        className: neutralButtonStyle,
+        hideOnMobile: true,
         onClick: () => {
           displayMedia.start();
           screenSharingStreaming.setActive(true);
@@ -267,6 +268,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Stop the screensharing",
+          hideOnMobile: true,
           onClick: () => displayMedia.stop(),
         }
       : {
@@ -274,6 +276,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Start the screensharing",
+          hideOnMobile: true,
           onClick: () => displayMedia.start(),
         },
     displayMedia.isEnabled
@@ -282,6 +285,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Disable screensharing stream",
+          hideOnMobile: true,
           onClick: () => displayMedia.disable(),
         }
       : {
@@ -289,6 +293,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Enable screensharing stream",
+          hideOnMobile: true,
           onClick: () => displayMedia.enable(),
         },
     screenSharingStreaming.trackId
@@ -297,6 +302,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Remove screensharing track",
+          hideOnMobile: true,
           onClick: () => screenSharingStreaming.removeTracks(),
         }
       : {
@@ -304,6 +310,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Add screensharing track",
+          hideOnMobile: true,
           onClick: () => displayMedia?.stream && screenSharingStreaming.addTracks(displayMedia?.stream),
         },
     screenSharingStreaming.trackMetadata?.active
@@ -312,6 +319,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Set 'active' metadata to 'false'",
+          hideOnMobile: true,
           onClick: () => screenSharingStreaming.setActive(false),
         }
       : {
@@ -319,6 +327,7 @@ const getManualControls = (
           icon: Screenshare,
           className: neutralButtonStyle,
           hover: "Set 'active' metadata to 'true'",
+          hideOnMobile: true,
           onClick: () => screenSharingStreaming.setActive(true),
         },
   ],
@@ -365,8 +374,15 @@ const MediaControlButtons: FC<Props> = (props: Props) => {
         <div className="inset-x-0 z-10 flex flex-wrap justify-center gap-x-4 rounded-t-md">
           {controls.map((group, index) => (
             <div key={index} className="flex justify-center gap-x-4">
-              {group.map(({ hover, onClick, className, id, icon }) => (
-                <MediaControlButton key={id} onClick={onClick} hover={hover} className={className} icon={icon} />
+              {group.map(({ hover, onClick, className, id, icon, hideOnMobile }) => (
+                <MediaControlButton
+                  key={id}
+                  onClick={onClick}
+                  hover={hover}
+                  className={className}
+                  icon={icon}
+                  hideOnMobile={hideOnMobile}
+                />
               ))}
             </div>
           ))}

010dffe2780228cda2806b2cf96d84885876a952
alexmaar <oo.marzec@gmail.com>
2023-02-17 09:35:33 +0100

    fix import error

--- assets/src/features/room-page/utils/remoteTrackToLocalTrack.tsx
+++ assets/src/features/room-page/utils/remoteTrackToLocalTrack.tsx
@@ -1,5 +1,5 @@
-import { TrackWithId } from "../../../pages/room/components/StreamPlayer/MediaPlayerPeersSection";
 import { Track } from "../../../pages/room/hooks/usePeerState";
+import { TrackWithId } from "../../../pages/types";
 
 export const remoteTrackToLocalTrack = (localPeer: Track | undefined): TrackWithId | null =>
   localPeer ? { ...localPeer, remoteTrackId: localPeer.trackId } : null;

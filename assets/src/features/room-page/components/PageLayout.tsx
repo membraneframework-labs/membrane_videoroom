f79c56ab96efb151ce017551652f9dcf42746990
Szymon Kania <26521377+ostatni5@users.noreply.github.com>
2023-03-13 16:11:48 +0100

    fix screen height on mobile

--- assets/src/features/room-page/components/PageLayout.tsx
+++ assets/src/features/room-page/components/PageLayout.tsx
@@ -8,19 +8,22 @@ const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
   const isHorizontalMobile = useHorizontalMobile();
   return (
     <>
-      {isHorizontalMobile && <BlockingScreen message="Turn your screen to resume the call." />}
-      <div
-        className={clsx(
-          "h-screen w-full bg-auto bg-center bg-no-repeat sm:bg-videoroom-background",
-          "bg-brand-sea-blue-100 font-rocGrotesk text-brand-dark-blue-500",
-          "flex flex-col items-center gap-y-4 p-4",
-          isHorizontalMobile && "invisible"
-        )}
-      >
-        <Navbar />
+      {isHorizontalMobile ? (
+        <BlockingScreen message="Turn your screen to resume the call." />
+      ) : (
+        <div
+          className={clsx(
+            "h-[100dvh] w-full bg-auto bg-center bg-no-repeat sm:bg-videoroom-background",
+            "bg-brand-sea-blue-100 font-rocGrotesk text-brand-dark-blue-500",
+            "flex flex-col items-center gap-y-4 p-4",
+            isHorizontalMobile && "invisible"
+          )}
+        >
+          <Navbar />
 
-        <div className="h-full w-full">{children}</div>
-      </div>
+          <div className="h-full w-full">{children}</div>
+        </div>
+      )}
     </>
   );
 };

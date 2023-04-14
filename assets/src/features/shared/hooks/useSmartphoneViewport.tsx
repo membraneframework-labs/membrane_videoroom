b17fec3567e27b6c7ddd691a8dd539372aa5d552
Paweł Kruczkiewicz <58188916+pkrucz00@users.noreply.github.com>
2023-04-14 07:28:09 +0200

    callbacks moved to useEffect
    Co-authored-by: Kamil Stasiak <kamilstasiaknew@gmail.com>

--- assets/src/features/shared/hooks/useSmartphoneViewport.tsx
+++ assets/src/features/shared/hooks/useSmartphoneViewport.tsx
@@ -10,35 +10,35 @@ const useSmartphoneViewport = (): ScreenInfo => {
   const [isSmartphone, setIsSmartphone] = useState<boolean | undefined>();
   const [isHorizontal, setIsHorizontal] = useState<boolean | undefined>();
 
-  const updateIsSmartphoneState = useCallback(() => {
-    if (!window.visualViewport) return;
-
-    const isCoarse = matchMedia("(pointer:coarse)").matches;
-    const hasMobileWidth = window.visualViewport.width <= MOBILE_WIDTH_BREAKPOINT;
-
-    const isLandscapedSmartphone =
-      screen.orientation.type.includes("landscape") && window.visualViewport.width <= MAX_MOBILE_WIDTH_BREAKPOINT; // iPhone 12 max  viewpor width
-    setIsSmartphone(isCoarse && (hasMobileWidth || isLandscapedSmartphone));
-  }, []);
-
-  const updateIsHorizontalOrientationState = useCallback(() => {
-    if (!screen) return;
-
-    setIsHorizontal(screen.orientation.type.includes("landscape"));
-  }, []);
-
-  const updateState = useCallback(() => {
-    updateIsSmartphoneState();
-    updateIsHorizontalOrientationState();
-  }, [updateIsSmartphoneState, updateIsHorizontalOrientationState]);
-
-  useEffect(() => {
+useEffect(() => {
     if (typeof window !== "undefined") {
+      const updateIsSmartphoneState = () => {
+        if (!window.visualViewport) return;
+
+        const isCoarse = matchMedia("(pointer:coarse)").matches;
+        const hasMobileWidth = window.visualViewport.width <= MOBILE_WIDTH_BREAKPOINT;
+
+        const isLandscapedSmartphone =
+          screen.orientation.type.includes("landscape") && window.visualViewport.width <= MAX_MOBILE_WIDTH_BREAKPOINT; // iPhone 12 max  viewpor width
+        setIsSmartphone(isCoarse && (hasMobileWidth || isLandscapedSmartphone));
+      }
+
+      const updateIsHorizontalOrientationState = () => {
+        if (!screen) return;
+
+        setIsHorizontal(screen.orientation.type.includes("landscape"));
+      }
+      
+      const updateState = () => {
+        updateIsSmartphoneState();
+        updateIsHorizontalOrientationState();
+      }
+      
       updateState();
       window.addEventListener("resize", updateState);
       return () => window.removeEventListener("resize", updateState);
     }
-  }, [updateState]);
+  }, []);
 
   return { isSmartphone, isHorizontal };
 };

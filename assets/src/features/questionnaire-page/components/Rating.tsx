b95edc16e1a1d07183494ea7d5d88f82022bce27
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-05-15 15:22:28 +0200

    Add hover

--- assets/src/features/questionnaire-page/components/Rating.tsx
+++ assets/src/features/questionnaire-page/components/Rating.tsx
@@ -1,34 +1,52 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Button from "../../shared/components/Button";
 import Star from "../icons/Star";
+import clsx from "clsx";
+
+type StarButtonProps = {
+  isActive: boolean;
+  isHoverActive: boolean;
+  onClick: () => void;
+  onHover: () => void;
+  onLeave: () => void;
+};
 
 type RatingProps = {
   title: string;
 };
 
-const StarButton = () => {
+const StarButton = ({ isActive, isHoverActive, onClick, onHover, onLeave }: StarButtonProps) => {
+  const fillColor = isHoverActive ? "fill-brand-dark-blue-200" : 
+                    isActive ? "fill-brand-dark-blue-300" :
+                    "fill-brand-white";
+
   return (
-    <Button
-      className="h-6 w-6"
-      onClick={() => {
-        console.log("Do something");
-      }}
-    >
-      <Star/>
-    </Button>
+    <div className="flex-none flex-grow-0" onClick={onClick} onMouseEnter={onHover} onMouseLeave={onLeave}>
+      <Star className={fillColor} />
+    </div>
   );
 };
 
 const Rating: FC<RatingProps> = ({ title }) => {
+  const [rateValue, setRateValue] = useState<number>(0);
+  const [hoverRateValue, setHoverRateValue] = useState<number | null>(null);
+
+  const ratingsFromOneToFive = Array.from({ length: 5 }, (_, i) => i + 1);
+
   return (
     <div className="flex flex-col flex-wrap gap-4 p-0">
-      <div className="text-lg font-aktivGrotesk ">{title}</div>
+      <div className="font-aktivGrotesk text-lg ">{title}</div>
       <div className="flex flex-row flex-wrap gap-3 rounded-xl bg-brand-white px-8 py-10">
-        <StarButton />
-        <StarButton />
-        <StarButton />
-        <StarButton />
-        <StarButton />
+        {ratingsFromOneToFive.map((rateIndex) => (
+          <StarButton
+            key={rateIndex}
+            isActive={hoverRateValue === null && rateIndex <= rateValue}
+            isHoverActive={hoverRateValue !== null && rateIndex <= hoverRateValue}
+            onClick={() => setRateValue(rateIndex)}
+            onHover={() => setHoverRateValue(rateIndex)}
+            onLeave={() => setHoverRateValue(null)}
+          />
+        ))}
       </div>
     </div>
   );

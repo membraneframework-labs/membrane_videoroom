b8feafc4a21fa1aa73ee6081b60a2f06d5201f14
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-06-07 12:32:39 +0200

    Comments about future deletion of this CL

--- assets/src/features/shared/hooks/useDynamicHeightResize.tsx
+++ assets/src/features/shared/hooks/useDynamicHeightResize.tsx
@@ -1,5 +1,12 @@
 import { useCallback, useEffect } from "react";
 
+// Inspired by: https://pqina.nl/blog/how-to-prevent-scrolling-the-page-on-ios-safari/
+
+// TODO when `dvh` will be more common please delete this module.
+// As a day of writing this comment, about 8 % of all mobile users use a version of Safari that do not support `dvh`.
+// When the amount will be significantly smaller, this module can be evaporated.
+// Reference: https://caniuse.com/?search=dvh
+
 const useDynamicHeightResize = () => {
   const changeCssHeightVariable = useCallback(() => {
     window.document.documentElement.style.setProperty("--window-inner-height", `${window.innerHeight}px`);

2a481a8647bf282b808b4eacc3289400b01568f1
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-06-16 12:01:01 +0200

    Persisting, when someone talked

--- assets/src/pages/room/hooks/useSortBySpeaking.tsx
+++ assets/src/pages/room/hooks/useSortBySpeaking.tsx
@@ -2,29 +2,46 @@ import { groupBy } from "ramda";
 import { MediaPlayerTileConfig, PeerTileConfig, isPeerTileConfig } from "../../types";
 import { useCallback, useState } from "react";
 
-// order:
+type date = number;
+
+// desired order:
 // 1. local tile first
 // 2. Screenshare tiles
 // 3. Those that have spoken sorted by when they have spoken
 // 4. The rest in an alphabetical order
 const useSortBySpeaking = (videoTiles: MediaPlayerTileConfig[]): MediaPlayerTileConfig[] => {
-    const [speakingHistory, setSpeakingHistory] = useState<Set<string>>(new Set());
-    const hasSpoken = useCallback((tileId: string) => speakingHistory.has(tileId), [speakingHistory]);
-    const addToHistory = useCallback((tileId: string) => {if (!hasSpoken(tileId)) setSpeakingHistory((oldHistory) => new Set(oldHistory).add(tileId))}, [setSpeakingHistory]);
-    // const removeFromHistory = useCallback((tileIds: string) => )
-    const sortRemoteTiles = (remoteTiles: PeerTileConfig[]) => {
-        const {speaking, rest} = groupBy( ({mediaPlayerId, isSpeaking}: PeerTileConfig) => hasSpoken(mediaPlayerId) || isSpeaking ? "speaking" : "rest")(remoteTiles);
-        const speakingTiles = speaking ?? [];
-        speakingTiles.map(({mediaPlayerId}) => mediaPlayerId).filter((id) => !hasSpoken(id)).forEach(addToHistory);
+  const [speakingHistory, setSpeakingHistory] = useState<Map<string, date>>(new Map());
+  const hasSpoken = useCallback((tileId: string) => speakingHistory.has(tileId), [speakingHistory]);
+  const getTimeLastSpoken = useCallback((tileId: string) => speakingHistory.get(tileId) ?? 0, [speakingHistory]);
+  const addToHistory = useCallback(
+    (tileId: string) => {
+      if (!hasSpoken(tileId)) setSpeakingHistory((oldHistory) => new Map(oldHistory).set(tileId, Date.now()));
+    },
+    [hasSpoken, setSpeakingHistory]
+  );
 
-        const restSortedByAlphabet = (rest ?? []).sort((a, b) => a.displayName.localeCompare(b.displayName));
-        return {speakingTiles, restSortedByAlphabet};
-    }
-    
-    const {localTiles, remoteTiles, screenshareTiles, } = groupBy(({typeName}: MediaPlayerTileConfig) => typeName === "local" ? "localTiles" : typeName === "remote" ? "remoteTiles" : "screenshareTiles")(videoTiles);
-    const {speakingTiles, restSortedByAlphabet} = sortRemoteTiles((remoteTiles ?? []).filter(isPeerTileConfig));
-    return [...localTiles, ...(screenshareTiles ?? []), ...speakingTiles, ...restSortedByAlphabet];
-}
+  const sortRemoteTiles = (remoteTiles: PeerTileConfig[]) => {
+    const { speaking, rest } = groupBy(({ mediaPlayerId, isSpeaking }: PeerTileConfig) =>
+      hasSpoken(mediaPlayerId) || isSpeaking ? "speaking" : "rest"
+    )(remoteTiles);
+    const newSpeakingTiles = (speaking ?? []).filter(({ mediaPlayerId }) => !hasSpoken(mediaPlayerId));
+    newSpeakingTiles.map(({ mediaPlayerId }) => mediaPlayerId).forEach(addToHistory);
+    const sortedByDate = (speaking ?? []).sort((a, b) =>
+      getTimeLastSpoken(a.mediaPlayerId) < getTimeLastSpoken(b.mediaPlayerId) ? 1 : -1
+    );
+    const tilesSortedByLastSpoken = [...newSpeakingTiles, ...sortedByDate];
 
+    const restSortedByAlphabet = (rest ?? []).sort((a, b) => a.displayName.localeCompare(b.displayName));
+    return { tilesSortedByLastSpoken, restSortedByAlphabet };
+  };
+
+  const { localTiles, remoteTiles, screenshareTiles } = groupBy(({ typeName }: MediaPlayerTileConfig) =>
+    typeName === "local" ? "localTiles" : typeName === "remote" ? "remoteTiles" : "screenshareTiles"
+  )(videoTiles);
+  const { tilesSortedByLastSpoken, restSortedByAlphabet } = sortRemoteTiles(
+    (remoteTiles ?? []).filter(isPeerTileConfig)
+  );
+  return [...localTiles, ...(screenshareTiles ?? []), ...tilesSortedByLastSpoken, ...restSortedByAlphabet];
+};
 
 export default useSortBySpeaking;

49dfbd8f0161cc254124f98ccb1ff5a97affc8b6
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-07-25 11:50:07 +0200

    Upgrade RTC Engine to v0.16.0 (#188)
    * Upgrade RTC Engine to v0.16.0
    * Update deps

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule VideoRoom.MixProject do
 
   defp deps do
     [
-      {:membrane_rtc_engine, "~> 0.14.0"},
+      {:membrane_rtc_engine, "~> 0.16.0"},
+      {:membrane_rtc_engine_webrtc, "~> 0.1.0"},
       {:membrane_rtc_engine_timescaledb, "~> 0.2.0", runtime: false},
       {:plug_cowboy, "~> 2.5"},
       {:phoenix, "~> 1.6.15"},
